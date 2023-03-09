1249f1940c1a91e2f93b47d93616fd409e051e40
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-03-09 10:49:28 +0200

    add ts in project

--- src/js/script.ts
+++ src/js/script.ts
@@ -3,12 +3,9 @@
 // import modules
 
 // import utils
-import { showAlert } from "./utils/showAlert";
-
-document.addEventListener('DOMContentLoaded', () => {
-    showAlert('test')
-})
+import { showAlert } from "./utils/showAlert"
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log('hello')
+    showAlert('hello')
 })
